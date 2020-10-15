fdc64db828bc4ec3c87265babea1cda334b6fe72
Turlybek Zhuranov <turlybekk@gmail.com>
2020-10-15 21:10:37 +0600

    feat: do reverse of integer number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let ans = "";
+  let num = Math.abs(n);
+  while (num >= 1){
+    ans += num%10;
+    num = Math.floor(num/10);
+  }
+
+  return ans;
 }
